e2473d44b106675285d1261bf50dd93b1933ecc2
YuraLen <92933581+YuraLen@users.noreply.github.com>
2023-02-28 18:25:41 +0200

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Task 1
+
+        int a = 10, b = 4;
+        String text = String.format("%d : %d = %d. Остача: %d", a, b, a / b, a % b);
+        System.out.println(text);
+
+        // Task 2
+
+        int num = 54;
+        if (num >= 10 && num <= 99){
+            int j = num % 10;
+            int k = (num / 10) % 10;
+            System.out.println("Сума цифр числа " + num + " рівна " + (j + k));
+        } else {
+            System.out.println("Число недвохзначне");
+        }
+
+        // Task 3
+
+        double degree = 38.6;
+
+        double res = Math.round(degree);
+        System.out.println(res);
     }
 }
